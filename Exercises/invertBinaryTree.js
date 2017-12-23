c38496ed10fb50387d20c50378cb2943ba91ba0f
Justin Kwok <jkwok0714@gmail.com>
2017-12-23 13:17:36 -0800

    Invert binary tree with test binary tree

---
+++ Exercises/invertBinaryTree.js
@@ -0,0 +1,55 @@
+// This problem was inspired by this original tweet by Max Howell:
+// Google: 90% of our engineers use the software you wrote (Homebrew)
+//  but you can’t invert a binary tree on a whiteboard so fuck off.
+
+let invertTree = (node) => {
+  if (!node) return;
+
+  node.left = invertTree(node.left);
+  node.right = invertTree(node.right);
+
+  [node.left, node.right] = [node.right, node.left];
+
+  return node;
+};
+
+// ==== SAMPLE TREE ====
+
+let BinaryTreeNode = function (value) {
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
+};
+
+BinaryTreeNode.prototype.print = function (depth = 0) {
+  console.log('D' + depth, this.value);
+
+  if (this.left) this.left.print(depth + 1);
+  if (this.right) this.right.print(depth + 1);
+};
+
+//==== TEST ====
+let testFunction = (tree) => {
+  console.log('Testing new tree');
+  tree.print();
+  console.log('Inverting');
+  invertTree(tree);
+  tree.print();
+};
+
+let noobTree = new BinaryTreeNode(4);
+noobTree.left = new BinaryTreeNode(5);
+noobTree.right = new BinaryTreeNode(6);
+
+testFunction(noobTree);
+// noobTree.print();
+
+let testTree = new BinaryTreeNode(5);
+
+testTree.left = new BinaryTreeNode(6);
+testTree.right = new BinaryTreeNode(4);
+testTree.left.left = new BinaryTreeNode(10);
+testTree.left.right = new BinaryTreeNode(12);
+
+testFunction(testTree);
+// testTree.print();
