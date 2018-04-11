a8bf0f281a54100205950e43dcf281f7a09a74a9
Justin Kwok <jkwok0714@gmail.com>
2018-04-10 21:09:51 -0700

    Printing stuff in C O L O R

--- Minitests/loggerTrace.js
+++ Minitests/loggerTrace.js
@@ -33,3 +33,60 @@ let func3 = () => {
 }
 
 func3();
+
+//Printing Stuff
+
+
+const COLORS = {
+	RED: '#f90727',
+  GREEN: '#31c438',
+  YELLOW: '#ffec5e',
+  GRAY: '#bababa'
+}
+
+let messages = [
+	{color: '#ddd', message: 'this is a message'},
+  {color: '#4286f4', message: 'good stuff'},
+  {color: '#f90727', message: 'really bad'}
+];
+
+let buildStringForCopy = (array) => {
+	return array.map(a => a.message).join('\n');
+};
+
+let addInfoLog = (...messageArgs) => {
+	messages.push({color: COLORS.GRAY, message: messageArgs.join(' ')});
+};
+let addSuccessLog = (...messageArgs) => {
+	messages.push({color: COLORS.GREEN, message: messageArgs.join(' ')});
+};
+let addErrorLog = (...messageArgs) => {
+	messages.push({color: COLORS.RED, message: messageArgs.join(' ')});
+};
+let addColoredLog = (color, ...messageArgs) => {
+	messages.push({color: color, message: messageArgs.join(' ')});
+};
+
+let printMessagesToChromeConsole = (array) => {
+	array.map(el => {
+  	console.log(`%c ${el.message}`, `color: ${el.color}`);
+  	return;
+  });
+};
+
+addInfoLog('Started the test phase');
+addInfoLog('Everything before was starter dummy data');
+let number = 69;
+addSuccessLog('Initiated a number', number);
+try {
+	let dummy = fool;
+} catch (e) {
+	addErrorLog('Got an error', e.message);
+}
+addColoredLog(COLORS.YELLOW, 'Here is a custom yellow log', number);
+addColoredLog('#3697c4', 'Non-constant blue log');
+let obj = { name: 'Tylero' };
+addInfoLog('What happens if we pass an object?', obj);
+addInfoLog('Closing test phase and printing');
+printMessagesToChromeConsole(messages);
+console.log('Built string of messages:\n', buildStringForCopy(messages));
