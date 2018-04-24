0354bf9195375325acd9a7194186423b31c30f85
Justin Kwok <jkwok0714@gmail.com>
2018-04-24 16:29:33 -0700

    Test for nodeJS opening pages

---
+++ Minitests/opener.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express');
+const opn = require('opn');
+const app = express();
+const PORT_NUMBER = 3000;
+
+/*
+// Specify the app to open in
+opn('http://sindresorhus.com', {app: 'firefox'});
+
+// Specify app arguments
+opn('http://sindresorhus.com', {app: ['google chrome', '--incognito']});
+*/
+
+app.get('/open', (req, res) => {
+  let requestedSite = req.query.site || 'google';
+  console.log('Request query', requestedSite);
+  opn(`https://www.${requestedSite}.com`, {app: 'google chrome'});
+  res.send('OPENEDED');
+});
+
+app.listen(PORT_NUMBER, () => console.log(`The Opener listening on port ${PORT_NUMBER}!`));
