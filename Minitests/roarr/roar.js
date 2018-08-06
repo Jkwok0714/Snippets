6209c750f9bc6988c2689c7ec97adde6d8d267d5
Justin Kwok <jkwok0714@gmail.com>
2018-08-06 14:09:58 -0700

    Logger tests

---
+++ Minitests/roarr/roar.js
@@ -0,0 +1,40 @@
+// import log from 'roarr';
+const log = require('roarr').default;
+
+// console.log('LOG:', log, '--');
+
+// log('foo');
+//
+// log('bar %s', 'baz');
+//
+// const debug = log.child({
+//   logLevel: 10
+// });
+//
+// debug('qux');
+//
+// debug({
+//   quuz: 'corge'
+// }, 'quux');
+
+const maxTicks = 100;
+let tick = 0;
+
+log('Initializing logger test');
+console.log('Normal consolo loggo');
+
+let timer = setInterval(() => {
+  if (tick >= maxTicks) {
+    clearInterval(timer);
+  } else {
+    tick++;
+    const randumb = Math.floor(Math.random() * 100);
+    log.info(`Creating log ${tick}: ${randumb}`);
+
+    if (randumb > 90) {
+      log.error('Fatal crash about to happen');
+      const obj = {};
+      console.log('heres obj', obj.hello.cool.stuff);
+    }
+  }
+}, 1000);
